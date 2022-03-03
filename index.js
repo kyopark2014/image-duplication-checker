009ef7e5aa0704fd10c38d103ac3c69b742b9df7
ksdyb.park <ksdyb@amazon.com>
2022-03-04 01:28:18 +0900

    update

--- index.js
+++ index.js
@@ -50,13 +50,14 @@ exports.handler = async (event, context) => {
       filename = uuid+ext;
     }
 
+    // extract fingerprint from the given image
+    let fingerprint;
     try {
-      const hashSum = crypto.createHash('sha256');
-      hashSum.update(body);
-      const hex = hashSum.digest('hex');
-      console.log('fingerprint = '+hex);
+      const hashSum = crypto.createHash('sha256');    
+      hashSum.update(body);      
+      fingerprint = hashSum.digest('hex');
       
-      //console.log('body = '+body);
+      console.log('fingerprint = '+fingerprint);
     } catch(error) {
       console.log(error);
       return;
@@ -92,37 +93,6 @@ exports.handler = async (event, context) => {
     return response;
 };
 
-function hexToBinary(s) {
-  const lookup = {
-    0: "0000",
-    1: "0001",
-    2: "0010",
-    3: "0011",
-    4: "0100",
-    5: "0101",
-    6: "0110",
-    7: "0111",
-    8: "1000",
-    9: "1001",
-    a: "1010",
-    b: "1011",
-    c: "1100",
-    d: "1101",
-    e: "1110",
-    f: "1111",
-    A: "1010",
-    B: "1011",
-    C: "1100",
-    D: "1101",
-    E: "1110",
-    F: "1111",
-  };
-  let ret = "";
-  for (let i = 0; i < s.length; i++) {
-    ret += lookup[s[i]];
-  }
-  return ret;
-}
 
 
 async function putItem(key, text) {
